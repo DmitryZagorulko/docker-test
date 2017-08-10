8a6e48dd54d50346a354653e45f08042c39919eb
DmitryZagorulko <IntensoDZ@yandex.ru>
2017-08-11 01:27:12 +0300

    add readme.md, correct configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# docker-compose
+## nginx:latest+php:5.6-fpm+mariadb:latest
+- clone this repo
+- mkdir database or correct docker-compose.yml
+- docker-compose up
--- code/index.php
+++ code/index.php
@@ -1,5 +1,4 @@
 <?php
-/* Подключение к базе данных MySQL, используя вызов драйвера */
 $host = '172.18.0.2';
 $db   = 'test';
 $user = 'root';
@@ -7,21 +6,21 @@ $pass = 'root';
 
 $dsn = "mysql:host=$host;dbname=$db";
 try {
-    $pdo = new PDO($dsn, $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-	var_dump($pdo);
-    // $sql = 'SELECT a, b FROM test';
-    // $qwe = $dbh->query($sql);
-    // $qwe->exec
-    $query = $pdo->prepare('SELECT * FROM test');
+	$pdo = new PDO($dsn, $user, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+	$sql = "CREATE TABLE test (
+	a INT NOT NULL AUTO_INCREMENT,
+	b varchar(40), PRIMARY KEY (a))";
+	$pdo->exec($sql);
+
+	$sql2 = "INSERT INTO test (b) VALUES ('test')";
+	$pdo->exec($sql2);
+
+	$query = $pdo->query("SELECT * FROM test");
 	$query->execute();
-	$result = $query->fetch(PDO::FETCH_ASSOC);
-print_r($result);
-
-
+	$result = $query->fetch();
+	print_r($result);
 } catch (PDOException $e) {
-    echo 'Подключение не удалось: ' . $e->getMessage();
+	echo 'Подключение не удалось: ' . $e->getMessage();
 }
-
-
-
 echo phpinfo();
